ab5ab78d23fcd21446be364366005556a8c4d023
nandiniranaa06-stack <nandiniranaa06@gmail.com>
2026-07-30 20:56:28 -0700

    match lowercase item import path

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Item = require('../models/Item');
+const Item = require('../models/item');
 
 router.get('/items', async (req, res) => {
     try {
