0aa12c5c77e69f87dc3173fac17f59287ea8ab48
nandiniranaa06-stack <nandiniranaa06@gmail.com>
2026-07-30 22:48:08 -0700

    fix: added item route endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -74,7 +74,7 @@ function App() {
         date,
         claimQuestion 
       };
-      const res = await axios.post("https://campuscrate-lost-found-project.onrender.com/", newItem);
+      const res = await axios.post("https://campuscrate-lost-found-project.onrender.com/item", newItem);
       setItems([res.data, ...items]);
       setTitle("");
       setDescription("");
