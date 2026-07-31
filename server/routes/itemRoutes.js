e1e62a38aa3adc60cb661356edef333b7537ac63
nandiniranaa06-stack <nandiniranaa06@gmail.com>
2026-07-30 23:47:47 -0700

    fix model path casing

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Item = require('../models/Item');
+const Item = require('../models/item');
 
 router.get('/', async (req, res) => {
     try {
